a64b75e7356922be636a41c6b0e875886fc67759
chulalak <56313629+chayanpx@users.noreply.github.com>
2020-12-06 04:29:56 +0700

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Game{
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Bank pun");
+        window.setContentPane(new GamePanel());
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.pack();
+        window.setVisible(true);
+    }
+
+}
